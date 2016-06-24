6f93f3ee25d028a30801b500715d8bc690e71b51
wujingpeng <wujingpeng@youzan.com>
2016-06-24 09:31:11 +0800

    add some code

---
+++ DEMO01-Layers/Layers/GradientNode.swift
@@ -0,0 +1,15 @@
+//
+//  GradientNode.swift
+//  Layers
+//
+//  Created by Broccoli on 16/6/23.
+//  Copyright © 2016年 Razeware LLC. All rights reserved.
+//
+
+import UIKit
+
+class GradientNode: ASDisplayNode {
+    class func drawRect(bounds: CGRect, withParameters parameters: NSObjectProtocol!, isCancelled isCancelledBlock: asdisplaynode_iscancelled_block_t!, isRasterizing: Bool) {
+        
+    }
+}
--- DEMO01-Layers/Layers/RainforestCardCell.swift
+++ DEMO01-Layers/Layers/RainforestCardCell.swift
@@ -16,7 +16,10 @@ class RainforestCardCell: UICollectionViewCell {
     let gradientView = GradientView()
     var featureImageSizeOptional: CGSize?
     var placeholderLayer = CALayer()
-    var backgroundImageNode: ASImageNode?
+    // 不能直接在这个属性上设置 layerBacked 会 Crash
+    //    var backgroundImageNode: ASImageNode?
+    var containerNode: ASDisplayNode?
+    var contentLayer: CALayer?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -48,6 +51,14 @@ class RainforestCardCell: UICollectionViewCell {
     //MARK: Cell Reuse
     override func prepareForReuse() {
         super.prepareForReuse()
+        
+        //        backgroundImageNode?.preventOrCancelDisplay = true
+        containerNode?.recursiveSetPreventOrCancelDisplay(true)
+        contentLayer?.removeFromSuperlayer()
+        
+        contentLayer = nil
+        //        backgroundImageNode = nil
+        containerNode = nil
     }
     
     //MARK: Cell Content
@@ -61,12 +72,61 @@ class RainforestCardCell: UICollectionViewCell {
         backgroundImageNode.contentMode = .ScaleAspectFill
         backgroundImageNode.layerBacked = true
         
+        backgroundImageNode.imageModificationBlock = {
+            input in
+            if input == nil {
+                return input
+            }
+            
+            if let blurredImage = input.applyBlurWithRadius(30, tintColor: UIColor(white:0.5, alpha: 0.3), saturationDeltaFactor: 1.8, maskImage: nil, didCancel: {
+                return false
+            }) {
+                return blurredImage
+            } else {
+                return image
+            }
+        }
+        
+        //MARK: - Container Node Creation Section
+        let containerNode = ASDisplayNode()
+        containerNode.layerBacked = true
+        containerNode.shouldRasterizeDescendants = true
+        containerNode.borderColor = UIColor(hue: 0, saturation: 0, brightness: 0.85, alpha: 0.2).CGColor
+        containerNode.borderWidth = 1
+        containerNode.addSubnode(backgroundImageNode)
+        
+        let featureImageNode = ASImageNode()
+        featureImageNode.layerBacked = true
+        featureImageNode.contentMode = .ScaleAspectFit
+        featureImageNode.image = image
+        containerNode.addSubnode(featureImageNode)
+        
+        let titleTextNode = ASTextNode()
+        titleTextNode.layerBacked = true
+        titleTextNode.backgroundColor = UIColor.clearColor()
+        titleTextNode.attributedString = NSAttributedString.attributedStringForTitleText(cardInfo.name)
+        containerNode.addSubnode(titleTextNode)
+        
+        let descriptionTextNode = ASTextNode()
+        descriptionTextNode.layerBacked = true
+        descriptionTextNode.backgroundColor = UIColor.clearColor()
+        descriptionTextNode.attributedString =
+            NSAttributedString.attributedStringForDescriptionText(cardInfo.description)
+        containerNode.addSubnode(descriptionTextNode)
+        
         // MARK: - Node Layout Section
-        backgroundImageNode.frame = FrameCalculator.frameForContainer(image.size)
+        containerNode.frame = FrameCalculator.frameForContainer(image.size)
+        backgroundImageNode.frame = FrameCalculator.frameForBackgroundImage(containerNode.bounds)
+        featureImageNode.frame = FrameCalculator.frameForFeatureImage(image.size, containerFrameWidth: containerNode.frame.size.width)
+        titleTextNode.frame = FrameCalculator.frameForTitleText(containerNode.bounds, featureImageFrame: featureImageNode.frame)
+        descriptionTextNode.frame = FrameCalculator.frameForDescriptionText(containerNode.bounds, featureImageFrame: featureImageNode.frame)
         
         //MARK: - Node Layer and Wrap Up Section
-        contentView.layer.addSublayer(backgroundImageNode.layer)
-        self.backgroundImageNode = backgroundImageNode
+        //        contentView.layer.addSublayer(backgroundImageNode.layer)
+        self.contentView.layer.addSublayer(containerNode.layer)
+        //        self.backgroundImageNode = backgroundImageNode
+        //        contentLayer = backgroundImageNode.layer
+        contentLayer = containerNode.layer
+        self.containerNode = containerNode
     }
-    
 }
