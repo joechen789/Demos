234bd3bcd3e57bb082955faf8ebde2bde3a1b2a4
broccoliii <broccoliii.me@gmail.com>
2016-06-28 07:53:44 +0800

    import SkyFloatingLabelTextField

--- DEMO02-LoginAnimation/LoginAnimation/MainViewController.swift
+++ DEMO02-LoginAnimation/LoginAnimation/MainViewController.swift
@@ -15,24 +15,4 @@ class MainViewController: UIViewController {
 
         view.backgroundColor = UIColor.orangeColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        dismissViewControllerAnimated(true, completion: nil)
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
