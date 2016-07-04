d38bb61b698ba5a176ff756aa9cc32a3ae3e7035
wujingpeng <wujingpeng@youzan.com>
2016-07-04 14:38:30 +0800

    Perfect example

--- UIImageMask/UIImageMask/ViewController.m
+++ UIImageMask/UIImageMask/ViewController.m
@@ -18,16 +18,47 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor orangeColor];
     
-    [self maskedImage];
+    UIImage *heightImage = [UIImage imageNamed:@"height"];
+    UIImageView *heightImageView = [[UIImageView alloc] initWithImage:heightImage];
+    [self maskImageView:heightImageView];
+    heightImageView.frame = CGRectMake(40, 40, heightImageView.bounds.size.width, heightImageView.bounds.size.height);
+    [self.view addSubview:heightImageView];
+    
+    UIImage *widthImage = [UIImage imageNamed:@"width"];
+    UIImageView *widthImageView = [[UIImageView alloc] initWithImage:widthImage];
+    [self maskImageView:widthImageView];
+    widthImageView.frame = CGRectMake(200, 40, widthImageView.bounds.size.width, widthImageView.bounds.size.height);
+    [self.view addSubview:widthImageView];
+    
+    UIImage *fatHeightImage = [UIImage imageNamed:@"fatHeight"];
+    UIImageView *fatHeightImageView = [[UIImageView alloc] initWithImage:fatHeightImage];
+    [self maskImageView:fatHeightImageView];
+    fatHeightImageView.frame = CGRectMake(40, 200, fatHeightImageView.bounds.size.width, fatHeightImageView.bounds.size.height);
+    [self.view addSubview:fatHeightImageView];
+    
+    UIImage *squareImage = [UIImage imageNamed:@"square"];
+    UIImageView *squareImageView = [[UIImageView alloc] initWithImage:squareImage];
+    [self maskImageView:squareImageView];
+    squareImageView.frame = CGRectMake(200, 200, squareImageView.bounds.size.width, squareImageView.bounds.size.height);
+    [self.view addSubview:squareImageView];
 }
 
-- (void)maskedImage {
-    UIImage *heightImage = [UIImage imageNamed:@"height"];
+- (void)maskImageView:(UIImageView *)backgroundImageView {
+   
     UIImage *maskImage = [UIImage imageNamed:@"FIMbubble"];
     
-    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:heightImage];
+    backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+    UIImage *image = backgroundImageView.image;
     
-    backgroundImageView.frame = CGRectMake(10, 40, 50, 150);
+    if (image.size.height >= image.size.width && image.size.height / image.size.width > 3) {
+        backgroundImageView.bounds = CGRectMake(0, 0, 50, 150);
+    } else if (image.size.height >= image.size.width && image.size.height / image.size.width < 3 && image.size.height / image.size.width > 1) {
+        backgroundImageView.bounds = CGRectMake(0, 0, image.size.width / image.size.height * 150, 150);
+    } else if (image.size.width >= image.size.height && image.size.width / image.size.height > 3) {
+        backgroundImageView.bounds = CGRectMake(0, 0, 150, 50);
+    } else {
+        backgroundImageView.bounds = CGRectMake(0, 0, 150, image.size.height / image.size.width * 150);
+    }
     
     UIEdgeInsets BubbleRightCapInsets = UIEdgeInsetsMake(maskImage.size.height * 0.5, maskImage.size.width * 0.5, maskImage.size.height * 0.5, maskImage.size.width * 0.5);
     NSLog(@"BubbleRightCapInsets Rect: --- %@", NSStringFromUIEdgeInsets(BubbleRightCapInsets));
@@ -47,11 +78,12 @@
     NSLog(@"contentsCenter Rect: --- %@", NSStringFromCGRect(mask.contentsCenter));
     backgroundImageView.layer.mask = mask;
     backgroundImageView.layer.masksToBounds = YES;
-    
-    [self.view addSubview:backgroundImageView];
 }
 
 - (UIImage *)maskedImage:(UIImage *)image mask:(UIImage *)maskImage {
+    
+    
+    
     CGImageRef maskImageRef = maskImage.CGImage;
     
     CGImageRef mask = CGImageMaskCreate(CGImageGetWidth(maskImageRef),
