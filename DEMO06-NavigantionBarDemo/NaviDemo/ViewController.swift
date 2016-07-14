1bc55149cb75e9856b24faf0d3c5469157304165
wujingpeng <wujingpeng@youzan.com>
2016-07-14 09:56:15 +0800

    fix bug

--- DEMO06-NavigantionBarDemo/NaviDemo/ViewController.swift
+++ DEMO06-NavigantionBarDemo/NaviDemo/ViewController.swift
@@ -14,11 +14,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.backgroundColor = UIColor.redColor()
+        let button = UIButton(frame: CGRect(x: 100, y: 100, width: 40, height: 40))
+        button.backgroundColor = UIColor.blueColor()
+        button.addTarget(self, action: #selector(foo1), forControlEvents: .TouchUpInside)
+        view.addSubview(button)
+        let longPressGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(foo))
+        longPressGestureRecognizer.numberOfTouchesRequired = 1
+        longPressGestureRecognizer.numberOfTapsRequired = 3
+        button.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    func foo1()  {
+        
+    }
+    func foo() {
+        
     }
     
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SecondViewController")
-        navigationController?.pushViewController(vc, animated: true)
-    }
+//    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+//        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("SecondViewController")
+//        navigationController?.pushViewController(vc, animated: true)
+//    }
 }
 
